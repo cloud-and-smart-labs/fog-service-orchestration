ec45c1c4993fe9b534498aa79d0216586022f147
Suvam Basak <suvambasak2008@gmail.com>
2022-03-11 23:38:36 +0530

    Method to pull YAML file added

--- orchestrator/io_operation.py
+++ orchestrator/io_operation.py
@@ -23,11 +23,18 @@ class FileHandler:
             file.write(yaml.dump(content))
 
 
-class Network():
+class NetworkHandler():
+    "Pull files from the internet"
+
     def __init__(self) -> None:
         self.context = ssl._create_unverified_context()
 
-    def fetch(self, url: str) -> str:
+    def pull(self, url: str) -> str:
+        "Pull as a text"
         with urlopen(url, context=self.context) as url:
-            data = url.read().decode()
-            return data
+            text = url.read().decode()
+            return text
+
+    def pull_yaml(self, url: str) -> dict:
+        "Pull as a dict"
+        return yaml.safe_load(self.pull(url))
