c282757059132f3cc3260b22ed6bca020cbe35e0
Suvam Basak <suvambasak2008@gmail.com>
2022-03-14 13:46:04 +0530

    Creates coroutine for each command

--- orchestrator/agent.py
+++ orchestrator/agent.py
@@ -9,12 +9,75 @@ from command_executor import CommandExecutor
 from tosca_processor import TOSCAProcessor
 
 
+async def template_builder(url: str):
+    "TOSCA Service Template creator"
+
+    printer = ColorPrint()
+    tosca = TOSCAProcessor()
+    printer.log("Creating TOSCA Service Template")
+    tosca.create_service_template(url)
+
+
+async def shell_command_executor(type: str, command: str, master: websockets):
+    "Shell command execute and send back the result"
+
+    executor = CommandExecutor()
+    printer = ColorPrint()
+
+    # Command execution
+    executor.set_command(command)
+    printer.log("Command Executing")
+    executor.execute()
+    printer.log("Waiting for execution result")
+    result = executor.result()
+    printer.log("Got results")
+
+    # Output view and send response
+    result["type"] = type
+    printer.output_viewer(result)
+
+    await master.send(json.dumps(result))
+
+
+async def command_handler(message: str, master: websockets):
+    "Incoming command handler"
+
+    printer = ColorPrint()
+
+    json_msg = json.loads(message)
+
+    if "cmd" == json_msg["type"]:
+        printer.command_viewer(json_msg["cmd"])
+        args = shlex.split(json_msg["cmd"])
+
+        match args[0]:
+            # TOSCA Template build command
+            case "build":
+                if 1 >= len(args):
+                    printer.log("Missing URL")
+                    return
+
+                asyncio.create_task(template_builder(args[1]))
+
+            # Cleanup with ansible-playbook
+            case "cleanup":
+                asyncio.create_task(shell_command_executor(
+                    json_msg["type"],
+                    "ansible-playbook cleanup.yaml",
+                    master)
+                )
+
+            # Shell Command
+            case _:
+                asyncio.create_task(shell_command_executor(
+                    json_msg["type"],
+                    json_msg["cmd"],
+                    master)
+                )
+
+
 async def client(host="localhost", port=80):
     "Websocket connection handler"
-
-    tosca = TOSCAProcessor()
-    printer = ColorPrint()
-    executor = CommandExecutor()
 
     async with websockets.connect(f"ws://{host}:{port}", ping_interval=None) as master:
         join = {
@@ -26,37 +89,7 @@ async def client(host="localhost", port=80):
         while True:
             try:
                 message = await master.recv()
-                json_msg = json.loads(message)
-
-                match json_msg["type"]:
-                    case "cmd":
-                        printer.command_viewer(json_msg["cmd"])
-
-                        args = shlex.split(json_msg["cmd"])
-
-                        # TOSCA Template build command
-                        if "build" == args[0]:
-                            if 1 >= len(args):
-                                printer.log("Missing URL")
-                                continue
-
-                            printer.log("Creating TOSCA Service Template")
-                            tosca.create_service_template(args[1])
-
-                        # Shell Command
-                        else:
-                            executor.set_command(json_msg["cmd"])
-                            printer.log("Command Executing")
-                            executor.execute()
-                            printer.log("Waiting for execution result")
-                            result = executor.result()
-                            printer.log("Got results")
-
-                            # Output view and send response
-                            result["type"] = json_msg["type"]
-                            printer.output_viewer(result)
-
-                            await master.send(json.dumps(result))
+                asyncio.create_task(command_handler(message, master))
 
             except websockets.ConnectionClosedOK:
                 print("Connection closed")
@@ -78,6 +111,9 @@ if __name__ == "__main__":
 
     if "PORT" in os.environ:
         PORT = int(os.environ["PORT"])
+
+    # Input file setup
+    TOSCAProcessor()
 
     try:
         while True:
